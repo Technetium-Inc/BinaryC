1d803ab7436494fda42891a6a8d624f8da15b281
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-03-05 09:24:34 +0000

    Add Compile Function and Update example

--- src/main.vb
+++ src/main.vb
@@ -8,8 +8,32 @@ Module BinaryC
 
         Return file
     End Function
+
+    Function Compile(text As String) As String
+        text = Replace(text, "0000", "Module")
+        text = Replace(text, "0001", "End")
+        text = Replace(text, "0010", "Sub")
+        text = Replace(text, "0011", "Main")
+        text = Replace(text, "0100", "Console.WriteLine")
+        text = Replace(text, "0101", "Console.ReadLine")
+        text = Replace(text, "0110", "Console.Write")
+        text = Replace(text, "0111", "Function")
+        text = Replace(text, "1000", "Return")
+        text = Replace(text, "1001", "As")
+        text = Replace(text, "1010", "String")
+        text = Replace(text, "1011", "args")
+        text = Replace(text, "1100", "Using")
+        text = Replace(text, "1101", "System")
+        text = Replace(text, "1110", "For")
+        text = Replace(text, "1111", "While")
+        Return text
+    End Function
+
     Sub Main(args As String())
         ' Read the file
-        Console.WriteLine(ReadFile(args(0)))
+        Dim code As String = ReadFile(args(0))
+        ' Lex the code
+        Dim toWrite As String = Compile(code)
+        Console.WriteLine(Compile(toWrite))
     End Sub
 End Module
