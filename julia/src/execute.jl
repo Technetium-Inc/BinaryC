9c85b19b8d2a31d118b6d70271f213c75937903a
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-27 15:35:00 +0530

    Completed defininf the set of keywords

--- julia/src/execute.jl
+++ julia/src/execute.jl
@@ -1,6 +1,21 @@
 
 const commands = Dict{String, String}(
-	"hi" => "bye"
+	"0000" => "Module",
+	"0001" => "End",
+	"0010" => "Sub",
+	"0011" => "Main",
+ 	"0100" => "Console.WriteLine"
+ 	"0101" =>  "Console.ReadLine"
+ 	"0110" => "Dim"
+ 	"0111" => "Function"
+ 	"1000" => "Return"
+ 	"1001" => "As"
+ 	"1010" => "String"
+ 	"1011" => "args"
+ 	"1100" => "Using"
+ 	"1101" => "System"
+ 	"1110" => "For"
+ 	"1111" => "While"
 )
 
 function execute_binary_c(execute_code::String)::String
